dd5cccee9a9dd0f4d444c1e1b74057b8e4c45b3b
Guy Chamberlain <guychamberlain@Guys-MacBook-Air.local>
2015-09-28 12:12:34 +1000

    stuff

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-	:publishable_key	=> ENV['PUBLISHABLE_KEY=pk_test_6pRNASCoBOKtIshFeQd4XMUh'],
-	:secret_key 			=> ENV['SECRET_KEY=sk_test_BQokikJOvBiI2HlWgH4olfQ2']
+	:publishable_key	=> 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
+	:secret_key 		=> 'sk_test_BQokikJOvBiI2HlWgH4olfQ2'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
