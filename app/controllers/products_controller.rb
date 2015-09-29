ad5a783a04b4ed220fe206d0d05c534a5ffae49b
Guy Chamberlain <guychamberlain@Guys-MacBook-Air.local>
2015-09-29 21:13:33 +1000

    stuff

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,21 +5,29 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     if params[:cat_id].present?
+    
       # Read by this category id
       cat = Category.find(params[:cat_id])
       @products = cat.products
       @search_desc = "Showing all " + cat.name
+    
     elsif params[:search].present? 
+      
       # Read based on the search param
       s = '%' + params[:search] + "%"
       @products = Product.where('lower(name) LIKE ?', s.downcase).all
+      
       # Also search through descriptions
       @products += Product.where('lower(description) LIKE ?', s.downcase).all
+      
       # Also search the categories for a matching name
       cats = Category.where('lower(name) LIKE ?', s.downcase).all
       cats.each do |cat|
          @products += cat.products
       end  
+
+      # Make sure products are unique in the array
+      @products = @products.uniq         # The coolest statement yet!!
       @search_desc = "Search results"
     else 
       @products = Product.all
